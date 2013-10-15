df4ad9bb640d03dfc3d847f52840e99efdfcb3de
Tim Visher <tvisher@monetate.com>
2013-10-15 16:15:02 -0400

    This is defaulting to the main server. EOM

--- elisp/marmalade.el
+++ elisp/marmalade.el
@@ -15,7 +15,6 @@
 
 ;; To use marmalade.el, you must set `marmalade-server' to the URL of the
 ;; Marmalade server to which pakages will be uploaded.
-;; TODO: This should default to the main server.
 
 ;;; License:
 
